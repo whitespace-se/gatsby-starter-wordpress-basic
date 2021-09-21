460bf141f4cf347534ce306dbcc091935a94ceae
Fredrik Johansson <jeanfredrik@gmail.com>
2021-09-21 16:14:11 +0200

    fix: fetch all tags

--- src/fragments/WP_TaxonomiesForHook.js
+++ src/fragments/WP_TaxonomiesForHook.js
@@ -2,7 +2,7 @@ import { graphql } from "gatsby";
 
 export const query = graphql`
   fragment WP_TaxonomiesForHook on WP {
-    tags {
+    tags(first: 10000) {
       nodes {
         name
         slug
